a5f26935003d1316c44e7cd664c36a38fb5de35c
arielcollatti <colatti2017@gmail.com>
2023-03-10 21:25:22 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("dezsegundos");
+Console.WriteLine();
+
+Console.WriteLine("0 ");Thread.Sleep(1000);
+Console.WriteLine("1 ");Thread.Sleep(1000);
+Console.WriteLine("2 ");Thread.Sleep(1000);
+Console.WriteLine("3 ");Thread.Sleep(1000);
+Console.WriteLine("4 ");Thread.Sleep(1000);
+Console.WriteLine("5 ");Thread.Sleep(1000);
+Console.WriteLine("6 ");Thread.Sleep(1000);
+Console.WriteLine("7 ");Thread.Sleep(1000);
+Console.WriteLine("8 ");Thread.Sleep(1000);
+Console.WriteLine("9 ");Thread.Sleep(1000);
+Console.WriteLine("10 ");
